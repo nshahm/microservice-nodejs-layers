202dcae97c4754c2d94fc39690316b5daf329f97
Arul Xavier Raj <arulxavierraj.selvaraj@objectfrontier.com>
2016-04-25 14:40:01 -0400

    unit test updated

--- test/dal/EmployeeModelTest.js
+++ test/dal/EmployeeModelTest.js
@@ -3,16 +3,8 @@
 var dbURI = 'mongodb://localhost/employees';
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
-var Dummy = mongoose.model('Dummy', new mongoose.Schema({ a: Number }));
-var clearDB = require('mocha-mongoose')(dbURI);
 var entity_employee_1 = require("entity-employee");
 describe("Example spec for a model", function () {
-    beforeEach(function (done) {
-        if (mongoose.connection.db) {
-            return done();
-        }
-        mongoose.connect(dbURI, done);
-    });
     var payload = {
         "employeeID": "1000002",
         "name": {
@@ -28,38 +20,64 @@ describe("Example spec for a model", function () {
             "zip": 12345
         }
     };
-    var employee = payload;
-    it("can be saved Employee", function (done) {
-        new entity_employee_1.EmployeeModel(employee).save(done);
+    beforeEach(function (done) {
+        if (mongoose.connection.db) {
+            return done();
+        }
+        mongoose.connect(dbURI, done);
     });
-    it("can be listed Employees", function (done) {
-        new entity_employee_1.EmployeeModel(employee).save(function (err, model) {
+    beforeEach(function (done) {
+        //delete all the customer records    
+        entity_employee_1.EmployeeModel.remove({}, function () {
+            done();
+        });
+    });
+    var employee = payload;
+    it("can be saved .....", function (done) {
+        entity_employee_1.EmployeeModel.create(employee, function (error) {
+            expect(error).to.not.exist;
+            done();
+        });
+    });
+    it("can be listed .....", function (done) {
+        entity_employee_1.EmployeeModel.create(employee, function (err, model) {
             expect(err).to.not.exist;
             entity_employee_1.EmployeeModel.find({}, function (err, docs) {
                 expect(err).to.not.exist;
-                expect(docs).to.have.length(1);
+                expect(docs).to.be.an("array").with.length(1);
                 done();
             });
         });
     });
-    it("can be findById", function (done) {
-        new entity_employee_1.EmployeeModel(employee).save(function (err, model) {
-            entity_employee_1.EmployeeModel.findById(model._id, function (err, docs) {
+    it("can be find by ID .....", function (done) {
+        entity_employee_1.EmployeeModel.create(employee, function (err, model) {
+            entity_employee_1.EmployeeModel.findOne({ _id: model._id }, function (err, docs) {
                 expect(err).to.not.exist;
-                //expect(docs).to.exist
-                expect(docs).to.not.equal('bar');
+                expect(docs.name.first).to.equal("John");
                 done();
             });
         });
     });
-    it("can be updated", function (done) {
-        new entity_employee_1.EmployeeModel(employee).save(function (err, model) {
+    it("can be updated .....", function (done) {
+        entity_employee_1.EmployeeModel.create(employee, function (err, model) {
             entity_employee_1.EmployeeModel.findById(model._id, function (err, docs) {
                 expect(err).to.not.exist;
                 docs.name.first = "John William";
-                entity_employee_1.EmployeeModel.update(docs._id, docs, function (err, result) {
+                var _id = docs._id;
+                entity_employee_1.EmployeeModel.update({ _id: _id }, docs, function (err, doc) {
                     expect(err).to.not.exist;
-                    expect(docs).to.have.length(1);
+                    expect(doc.ok).to.equal(1);
+                });
+                done();
+            });
+        });
+    });
+    it("can be deleted .....", function (done) {
+        entity_employee_1.EmployeeModel.create(employee, function (err, model) {
+            entity_employee_1.EmployeeModel.findById(model._id, function (err, docs) {
+                expect(err).to.not.exist;
+                entity_employee_1.EmployeeModel.remove({ _id: docs._id }, function (err) {
+                    expect(err).to.not.exist;
                 });
                 done();
             });
--- test/dal/EmployeeModelTest.ts
+++ test/dal/EmployeeModelTest.ts
@@ -3,21 +3,11 @@
 var dbURI = 'mongodb://localhost/employees';
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
-var Dummy = mongoose.model('Dummy', new mongoose.Schema({ a: Number }));
-var clearDB = require('mocha-mongoose')(dbURI);
 import {EmployeeModel, IEmployeeModel} from "entity-employee";
 
 describe("Example spec for a model", function () {
 
-    beforeEach(function (done) {
-
-        if (mongoose.connection.db) {
-            return done();
-        }
-        mongoose.connect(dbURI, done);
-    });
-
-    const payload = {
+   const payload = {
         "employeeID": "1000002",
         "name": {
             "first": "John",
@@ -33,51 +23,85 @@ describe("Example spec for a model", function () {
         }
     };
 
+    beforeEach(function (done) {
+
+        if (mongoose.connection.db) {
+            return done();
+        }
+        mongoose.connect(dbURI, done);
+    });
+
+    beforeEach(function (done) {
+        //delete all the customer records    
+        EmployeeModel.remove({}, function () {
+            done();
+        });
+    });
+
+
+
     var employee: IEmployeeModel = <IEmployeeModel>payload;
 
-    it("can be saved Employee", function (done) {
-        new EmployeeModel(employee).save(done);
+    it("can be saved .....", function (done) {
+        EmployeeModel.create(employee, function(error) {
+            expect(error).to.not.exist;
+            done();
+        });
     });
 
-    it("can be listed Employees", function (done) {
+    it("can be listed .....", function (done) {
 
-
-        new EmployeeModel(employee).save(function (err, model) {
+        EmployeeModel.create(employee, function (err, model) {
 
             expect(err).to.not.exist;
-            EmployeeModel.find({}, function (err, docs) {
+            EmployeeModel.find({}, function (err, docs: any) {
 
                 expect(err).to.not.exist;
-                expect(docs).to.have.length(1);
+                expect(docs).to.be.an("array").with.length(1);
                 done();
             });
         });
     });
 
-    it("can be findById", function (done) {
+    it("can be find by ID .....", function (done) {
 
-        new EmployeeModel(employee).save(function (err, model: any) {
-            EmployeeModel.findById(model._id, function (err, docs) {
+        EmployeeModel.create(employee, function (err, model: any) {
+            EmployeeModel.findOne({ _id: model._id }, function (err, docs) {
 
                 expect(err).to.not.exist;
-                //expect(docs).to.exist
-                expect(docs).to.not.equal('bar');
+                expect(docs.name.first).to.equal("John");
                 done();
             });
         });
     });
 
-    it("can be updated", function (done) {
+    it("can be updated .....", function (done) {
 
-        new EmployeeModel(employee).save(function (err, model: any) {
+        EmployeeModel.create(employee, function (err, model: any) {
 
             EmployeeModel.findById(model._id, function (err, docs: any) {
 
                 expect(err).to.not.exist;
                 docs.name.first = "John William"
-                EmployeeModel.update(docs._id, docs, function (err, result) {
+
+                var _id = docs._id;
+                EmployeeModel.update({ _id: _id }, docs, function (err, doc: any) {
                     expect(err).to.not.exist;
-                    expect(docs).to.have.length(1);
+                    expect(doc.ok).to.equal(1);
+                });
+                done();
+            });
+        });
+    });
+
+    it("can be deleted .....", function (done) {
+
+        EmployeeModel.create(employee, function (err, model: any) {
+            EmployeeModel.findById(model._id, function (err, docs: any) {
+
+                expect(err).to.not.exist;
+                EmployeeModel.remove({ _id: docs._id }, function (err) {
+                    expect(err).to.not.exist;
                 });
                 done();
             });
