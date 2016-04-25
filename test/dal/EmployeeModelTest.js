a5aa7562fd1b6f6f93c792170e9484a6a96c8115
Arul Xavier Raj <arulxavierraj.selvaraj@objectfrontier.com>
2016-04-24 22:52:49 -0400

    Test case updated

--- test/dal/EmployeeModelTest.js
+++ test/dal/EmployeeModelTest.js
@@ -3,6 +3,7 @@
 var dbURI = 'mongodb://localhost/employees';
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
+var Dummy = mongoose.model('Dummy', new mongoose.Schema({ a: Number }));
 var clearDB = require('mocha-mongoose')(dbURI);
 var entity_employee_1 = require("entity-employee");
 describe("Example spec for a model", function () {
@@ -15,7 +16,7 @@ describe("Example spec for a model", function () {
     var payload = {
         "employeeID": "1000002",
         "name": {
-            "first": "John William",
+            "first": "John",
             "last": "Patrick"
         },
         "address": {
@@ -28,15 +29,38 @@ describe("Example spec for a model", function () {
         }
     };
     var employee = payload;
-    it("can be saved", function (done) {
+    it("can be saved Employee", function (done) {
         new entity_employee_1.EmployeeModel(employee).save(done);
     });
-    it("can be listed", function (done) {
+    it("can be listed Employees", function (done) {
         new entity_employee_1.EmployeeModel(employee).save(function (err, model) {
             expect(err).to.not.exist;
             entity_employee_1.EmployeeModel.find({}, function (err, docs) {
                 expect(err).to.not.exist;
                 expect(docs).to.have.length(1);
+                done();
+            });
+        });
+    });
+    it("can be findById", function (done) {
+        new entity_employee_1.EmployeeModel(employee).save(function (err, model) {
+            entity_employee_1.EmployeeModel.findById(model._id, function (err, docs) {
+                expect(err).to.not.exist;
+                //expect(docs).to.exist
+                expect(docs).to.not.equal('bar');
+                done();
+            });
+        });
+    });
+    it("can be updated", function (done) {
+        new entity_employee_1.EmployeeModel(employee).save(function (err, model) {
+            entity_employee_1.EmployeeModel.findById(model._id, function (err, docs) {
+                expect(err).to.not.exist;
+                docs.name.first = "John William";
+                entity_employee_1.EmployeeModel.update(docs._id, docs, function (err, result) {
+                    expect(err).to.not.exist;
+                    expect(docs).to.have.length(1);
+                });
                 done();
             });
         });
--- test/dal/EmployeeModelTest.ts
+++ test/dal/EmployeeModelTest.ts
@@ -1,55 +1,87 @@
 /// <reference path="../../typings/main.d.ts" />
 
-const dbURI    = 'mongodb://localhost/employees';
-const expect   = require('chai').expect;
-import mongoose = require('mongoose');
-const clearDB  = require('mocha-mongoose')(dbURI);
+var dbURI = 'mongodb://localhost/employees';
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+var Dummy = mongoose.model('Dummy', new mongoose.Schema({ a: Number }));
+var clearDB = require('mocha-mongoose')(dbURI);
 import {EmployeeModel, IEmployeeModel} from "entity-employee";
- 
-describe("Example spec for a model", function() {
-  
-  beforeEach(function(done) {
-    
-      if (mongoose.connection.db) {
-        return done();
-      }
-      mongoose.connect(dbURI, done);
-  });
-  
-  const payload = {
-      "employeeID": "1000002",
-      "name": {
-          "first": "John William",
-          "last": "Patrick"
-      },
-      "address": {
-          "lines": [
-              "1234 Delk RD SW"
-          ],
-          "city": "Atlanta",
-          "state": "GA",
-          "zip": 12345
-      }
-  };
-  
-  var employee: IEmployeeModel = <IEmployeeModel>payload;
- 
-  it("can be saved", function(done) {    
-    new EmployeeModel(employee).save(done);
-  });
-  
-  it("can be listed", function(done) {
-    
-    new EmployeeModel(employee).save(function(err, model){
-      
-        expect(err).to.not.exist;
-        
-        EmployeeModel.find({}, function(err, docs){
-          
+
+describe("Example spec for a model", function () {
+
+    beforeEach(function (done) {
+
+        if (mongoose.connection.db) {
+            return done();
+        }
+        mongoose.connect(dbURI, done);
+    });
+
+    const payload = {
+        "employeeID": "1000002",
+        "name": {
+            "first": "John",
+            "last": "Patrick"
+        },
+        "address": {
+            "lines": [
+                "1234 Delk RD SW"
+            ],
+            "city": "Atlanta",
+            "state": "GA",
+            "zip": 12345
+        }
+    };
+
+    var employee: IEmployeeModel = <IEmployeeModel>payload;
+
+    it("can be saved Employee", function (done) {
+        new EmployeeModel(employee).save(done);
+    });
+
+    it("can be listed Employees", function (done) {
+
+
+        new EmployeeModel(employee).save(function (err, model) {
+
             expect(err).to.not.exist;
-            expect(docs).to.have.length(1);
-            done();
+            EmployeeModel.find({}, function (err, docs) {
+
+                expect(err).to.not.exist;
+                expect(docs).to.have.length(1);
+                done();
+            });
         });
     });
-  });  
+
+    it("can be findById", function (done) {
+
+        new EmployeeModel(employee).save(function (err, model: any) {
+            EmployeeModel.findById(model._id, function (err, docs) {
+
+                expect(err).to.not.exist;
+                //expect(docs).to.exist
+                expect(docs).to.not.equal('bar');
+                done();
+            });
+        });
+    });
+
+    it("can be updated", function (done) {
+
+        new EmployeeModel(employee).save(function (err, model: any) {
+
+            EmployeeModel.findById(model._id, function (err, docs: any) {
+
+                expect(err).to.not.exist;
+                docs.name.first = "John William"
+                EmployeeModel.update(docs._id, docs, function (err, result) {
+                    expect(err).to.not.exist;
+                    expect(docs).to.have.length(1);
+                });
+                done();
+            });
+        });
+    });
+
 });
