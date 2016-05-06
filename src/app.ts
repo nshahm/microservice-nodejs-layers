c79ffd4ebe090327f5a011c488acecbbcee0cf95
Carestream <carestream@Aruls-MBP.corp.carestreamhealth.com>
2016-05-06 15:35:59 -0400

    Logger config added

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,26 @@ import * as express from "express";
 import * as BodyParser from "body-parser";
 import * as CookieParser from "cookie-parser";
 import * as morgan  from "morgan";
+import * as Config from "config";
 import { getInstance } from "./inversify.config";
 import { Logger } from "asd-microservice-logger";
 import { IAPI } from "./api/IAPI";
 import "./config/Mongodb";
 
 let app: any = express(),
-    api: IAPI = getInstance<IAPI>("IAPI");
-
+    api: IAPI = getInstance<IAPI>("IAPI"),
+    loggerConfig: any = Config.get("logger");
+    
 /**
  * Initialize Logger
  */
-Logger.init("", false);
+Logger.init(loggerConfig);
 
 /** App Middlewares */
+app.use(function (req, res, next) {
+    next();
+});
+
 app.use(BodyParser.urlencoded({ extended: false }));
 app.use(BodyParser.json());
 app.use(CookieParser());
